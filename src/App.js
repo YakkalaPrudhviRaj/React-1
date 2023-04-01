856181efaf6f74f791d2d7b73deabefc92656c2b
YakkalaPrudhviRaj <116963544+YakkalaPrudhviRaj@users.noreply.github.com>
2023-04-01 19:32:39 +0530

    .

---
+++ src/App.js
@@ -0,0 +1,34 @@
+
+import { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+class App extends Component {
+  state = {
+    data: "",
+  }
+  changeHandler(event) {
+    event.preventDefault();
+    this.setState({
+      data: event.target.value
+    })
+  }
+  render() {
+    return (
+
+      <div className="container">
+        <div className="input-group mb-3">
+        <div className='one'>
+          <input type="text" className="form-control ,center-block" placeholder="Type here..." aria-label="Username" aria-describedby="basic-addon1" onChange={(e) => this.
+            changeHandler(e)}></input>
+            </div>
+        </div>
+
+        <div style={{ textAlign: "center", justifyContent: "center", padding: "10px" }}>
+          {this.state.data}
+        </div>
+      </div >
+    );
+  }
+}
+
+export default App;
